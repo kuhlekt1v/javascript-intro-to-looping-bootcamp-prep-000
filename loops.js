4948f01e7b44befa20263145fb263e8a05c94f04
Cody Sheridan <csheridan994@gmail.com>
2018-04-20 10:06:08 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -18,12 +18,11 @@ function whileLoop(n){
     return('done')
     }
 function doWhileLoop(array) {
-  function maybeTrue() {
-    return Math.random() >= 0.5
-  }
-
-  do {
+   function maybeTrue() {
+     return Math.random() >= 0.5
+   }
+   do {
     array.pop();
-  } while (array.length > -1 && maybeTrue());
+   } while (array.length > -1 && maybeTrue());
     return array;
 }
